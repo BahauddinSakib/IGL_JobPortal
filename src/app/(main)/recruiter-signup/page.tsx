c79ad1ba398865a85f22d4d383f7638fecdab1ad
Bahauddin Sakib <98357419+BahauddinSakib@users.noreply.github.com>
2025-12-06 16:57:26 +0600

    Update page.tsx

--- src/app/(main)/recruiter-signup/page.tsx
+++ src/app/(main)/recruiter-signup/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useAuth } from '@/contexts/AuthContext';
+import { AuthContext } from "@/contexts/AuthContext";
 import Navbar from "../componants/navbarDark";
 
 export default function RecruiterSignup(){
@@ -313,4 +313,5 @@ export default function RecruiterSignup(){
             </section>
         </>
     )
+
 }
